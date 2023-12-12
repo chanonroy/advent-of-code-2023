d73aa5eecd23eea3836d95057beadc39792aaf9b
Chanon Roy <chanonroy@gmail.com>
2023-12-12 00:40:08 +0000

    move files

--- code/day_1/one.rb
+++ code/day_1/one.rb
@@ -1,3 +1,4 @@
+# typed: true
 # --- Day 1: Trebuchet?! ---
 # Something is wrong with global snow production, and you've been selected to take a look. The Elves have even given you a map; on it, they've used stars to mark the top fifty locations that are likely to be having problems.
 #
@@ -45,5 +46,3 @@ def find_first_digits(line)
 
   "#{start_digit}#{end_digit}".to_i
 end
-
-puts calibration_document_sum('1.txt')
--- code/day_1/one_test.rb
+++ code/day_1/one_test.rb
@@ -1,14 +1,15 @@
+# typed: true
 # frozen_string_literal: true
 
 require 'minitest/autorun'
 require 'minitest/reporters'
-require_relative '1'
+require_relative 'one'
 
 Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(color: true)]
 
 # Dec 1
 class One < Minitest::Test
   def test
-    assert_equal 54_708, calibration_document_sum('1.txt')
+    assert_equal 54_708, calibration_document_sum('input.txt')
   end
 end
