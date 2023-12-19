93c1dc1572a1418e70a77a5909ec5994e177157b
Chanon Roy <chanonroy@gmail.com>
2023-12-19 12:01:16 +0000

    day six test fix

--- code/day_6/six_test.rb
+++ code/day_6/six_test.rb
@@ -8,10 +8,10 @@ Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new(color: true)]
 
 # Dec 6
 class Four < Minitest::Test
-  def test_day_four_sample
-    assert_equal 288, day_four('sample.txt')
+  def test_day_six_sample
+    assert_equal 288, day_six('sample.txt')
   end
-  def test_day_four_real
-    assert_equal 512295, day_four('real.txt')
+  def test_day_six_real
+    assert_equal 512295, day_six('real.txt')
   end
 end
